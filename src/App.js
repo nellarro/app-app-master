a222812147f798aa891330f20330b19c0eced16a
Jenell Pizarro <nellarro@gmail.com>
2016-08-22 18:07:09 -0400

    First version of app without using create react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.sass'
+
+class App extends React.Component {
+
+  render () {
+    return <div>
+      <h1>App App</h1>
+      <h5>the boilerplate</h5>
+    </div>
+  }
+}
+
+export default App
